fe36f744fa3374c326029a8929bcb2ce8afad670
heo won chul <heowc1992@gmail.com>
2017-09-10 17:30:49 +0900

    Issue 10172

---
+++ src/main/java/com/tistory/heowc/baekjoon/Issue10172.java
@@ -0,0 +1,14 @@
+package com.tistory.heowc.baekjoon;
+
+public class Issue10172 {
+
+	public static void main(String [] args) {
+		System.out.println(
+			"|\\_/|\n" +
+			"|q p|   /}\n" +
+			"( 0 )\"\"\"\\\n" +
+			"|\"^\"`    |\n" +
+			"||_/=\\\\__|"
+		);
+	}
+}
