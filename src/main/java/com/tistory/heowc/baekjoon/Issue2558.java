e6d4f619bd83517cb3459669c11ac5b46762bf1a
heo won chul <heowc1992@gmail.com>
2017-09-11 13:43:14 +0900

    Issue 2558

---
+++ src/main/java/com/tistory/heowc/baekjoon/Issue2558.java
@@ -0,0 +1,15 @@
+package com.tistory.heowc.baekjoon;
+
+import java.util.Scanner;
+
+public class Issue2558 {
+
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int firstNumber = scanner.nextInt();
+		int secondNumber = scanner.nextInt();
+
+		System.out.println(firstNumber + secondNumber);
+	}
+}
