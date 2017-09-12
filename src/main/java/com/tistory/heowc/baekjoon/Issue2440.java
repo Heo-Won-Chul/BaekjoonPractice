570b5326374305e72983725375aef3462e0d772e
heo won chul <heowc1992@gmail.com>
2017-09-12 15:20:53 +0900

    Issue 2440

---
+++ src/main/java/com/tistory/heowc/baekjoon/Issue2440.java
@@ -0,0 +1,25 @@
+package com.tistory.heowc.baekjoon;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Issue2440 {
+
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		final int numberN = scanner.nextInt();
+
+		final String result = IntStream.range(0, numberN)
+										.mapToObj(value -> createStar(numberN, value))
+										.collect(Collectors.joining("\n"));
+		System.out.print(result);
+	}
+
+	private static String createStar(final int n, final int limit) {
+		return IntStream.range(0, n)
+						.limit(n - limit)
+						.mapToObj(value -> "*")
+						.reduce((a, b) -> a + b).get();
+	}
+}
