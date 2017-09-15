e7813b953bc8e834a379ca7903968122cc417b34
heo won chul <heowc1992@gmail.com>
2017-09-15 12:24:01 +0900

    Issue 11654

---
+++ src/main/java/com/tistory/heowc/baekjoon/Issue11654.java
@@ -0,0 +1,11 @@
+package com.tistory.heowc.baekjoon;
+
+import java.util.Scanner;
+
+public class Issue11654 {
+
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print((int) scanner.nextLine().charAt(0));
+	}
+}
