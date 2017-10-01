3cfd70cde1b800b96c8f5ce2bb6178b4909e13a1
heo won chul <heowc1992@gmail.com>
2017-10-01 17:34:10 +0900

    Issue 2438 + 2748

--- src/main/java/com/tistory/heowc/baekjoon/Issue2747.java
+++ src/main/java/com/tistory/heowc/baekjoon/Issue2747.java
@@ -1,17 +1,18 @@
 package com.tistory.heowc.baekjoon;
 
-import java.util.*;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Issue2747 {
 
 	public static void main(String [] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int [] fibonacciArray = new int[n + 1];
+		int n = new Scanner(System.in).nextInt();
+		BigInteger[] fibonacciArray = new BigInteger[91];
 
-		fibonacciArray[1] = 1;
-		for (int i = 2; i < n + 1; i++) {
-			fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+		fibonacciArray[0] = BigInteger.ZERO;
+		fibonacciArray[1] = BigInteger.ONE;
+		for (int i = 2; i <= n; i++) {
+			fibonacciArray[i] = fibonacciArray[i - 1].add(fibonacciArray[i - 2]);
 		}
 		System.out.print(fibonacciArray[n]);
 	}
